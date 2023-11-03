5e20b3fe5eb9ceaaf65c89b70f00d42d0be0f8c8
Z <skywalker@yoda.io>
2023-11-03 19:44:15 +0000

    A1

--- src/TTT/A1.hs
+++ src/TTT/A1.hs
@@ -3,49 +3,74 @@ module TTT.A1 where
 import Data.Char (toUpper)
 
 -- Q#01
-
-_SIZE_ = undefined
+_SIZE_ :: Int
+_SIZE_ = 3
 
 -- Q#02
-
-_DISPLAY_LOGO_ = undefined
+_DISPLAY_LOGO_ :: Bool
+_DISPLAY_LOGO_ = True
 
 -- Q#03
-
-convertRowIndex = undefined
+convertRowIndex :: Char -> Int
+convertRowIndex = (\x -> x - 65) . fromEnum . toUpper
 
 -- Q#04
-
-_INVALID_MOVE_ = undefined
+_INVALID_MOVE_ :: (Int, Int)
+_INVALID_MOVE_ = (-1, -1)
 
 -- Q#05
-
-_SEP_ = undefined
+_SEP_ :: [Char]
+_SEP_ = "_|_"
 
 -- Q#06
 
-data Square
+data Square = X | O | Empty deriving (Show, Eq, Read)
 
 -- Q#07
 
-data GameState
+data GameState = XWins | OWins | Draw | InProgress deriving (Show, Eq, Read)
 
 -- Q#08
+type Player = Square
+type Row = [Square]
+type Line = [Square]
+type Board = [Row]
+type Move = (Int, Int)
 
 -- Q#09
+getFirstPlayer :: Bool -> Player
+getFirstPlayer x = if x then X else O
 
-getFirstPlayer = undefined
-
-getFirstPlayer_ = undefined
+getFirstPlayer_ :: Bool -> Player
+getFirstPlayer_ x
+  | x == True = X
+  | otherwise = O
 
 -- Q#10
 
-showGameState = undefined
+showGameState :: GameState -> String
+showGameState g = case g of
+                    XWins -> "X is the winner"
+                    OWins -> "Y is the winner"
+                    Draw -> "It's a draw"
+                    InProgress -> "Game in progress"
+
+showGameState' :: GameState -> String
+showGameState' = show
+
 
 -- Q#11
+switchPlayer :: Player -> Player
+switchPlayer X = O
+switchPlayer O = X
+switchPlayer _ = Empty
 
-switchPlayer = undefined
 
 -- Q#12
+showSquare :: Square -> String
+showSquare X = "X"
+showSquare O = "O"
+showSquare _ = "_"
 
-showSquare = undefined
+showSquare' :: Square -> String
+showSquare' x = if x == Empty then "_" else show x  
